618b6c05de161a4601dc7f3cfd7de6ca618221f0
Darisam <98291008+Darisam@users.noreply.github.com>
2022-04-17 14:11:09 +0200

    just trying stuff out

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ const app = express();
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', {
+/*mongoose.connect('mongodb://localhost:27017/myFlixDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+});*/
 
 const allowedOrigins = ['http:/localhost:8080'];
 
