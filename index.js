6e56fee9597a15324a3135624e34562e26a66b4b
Darisam <98291008+Darisam@users.noreply.github.com>
2022-04-17 18:06:04 +0200

    Add hashing to updating users

--- index.js
+++ index.js
@@ -211,12 +211,14 @@ app.put(
       return res.status(422).json({ errors: errors.array() });
     }
 
+    let hashedPassword = Users.hashPassword(req.body.Password);
+
     Users.findOneAndUpdate(
       { Username: req.params.Username },
       {
         $set: {
           Username: req.body.Username,
-          Password: req.body.Password,
+          Password: hashedPassword,
           Email: req.body.Email,
           Birthday: req.body.Birthday,
         },
